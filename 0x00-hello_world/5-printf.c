815b863b5d5f6cc5249360527a7935844903d132
sunkari <sunkarioe7@gmail.com>
2023-07-14 08:20:20 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - printing a line of code with the put function
- * Return: 0(success)
+ * main - printing a line of code with the printf function
+ * Return: 0(Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
