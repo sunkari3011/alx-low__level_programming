d9097f1ef34f454cc9d3d14d50953d07303602a2
sunkari <sunkarioe7@gmail.com>
2023-07-14 08:12:20 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printing a line of code with the put function
+ * Return: 0(success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
